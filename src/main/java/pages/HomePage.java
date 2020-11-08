35918e4aa4e0c1ed0326607c5eb5d8f90e947a89
nbatista13 <nailton.oliveira01@gmail.com>
2020-11-08 16:27:28 -0300

    criar evidencia - metodo de Screenshot e metodo de caracteres randomicos

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,43 +1,44 @@
 package pages;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
 	private WebDriver driver;
-
-	/*
-	 * List<WebElement> listaCarros = new ArrayList(); private By carros =
-	 * By.xpath("//h2[@class='esquerda titulo_anuncio']"); private By
-	 * descricaoCarros = By.xpath("//h2[@class='esquerda titulo_anuncio']"); private
-	 * By precoCarros = By.xpath("//h3[@class='direita preco_anuncio']");
-	 */
 
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 	}
 
 	public ListaAnunciosCarroPage acessaLista() {
+		// Clicar campo Marca
 		driver.findElement(By.xpath(
 				"//body/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/button[1]/span[1]"))
 				.click();
+		// Selecionar opção Audi
+		driver.findElement(By.xpath(
+				"/html[1]/body[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"))
+				.sendKeys("Audi");
 		driver.findElement(By.xpath("//*[@id=\"buscaForm\"]/div[2]/div[1]/div/div/div/div/div/ul/li[4]/a/span[1]"))
 				.click();
+
+		// Selecionar modelo A1
 		driver.findElement(By.xpath(
 				"//body/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/button[1]"))
 				.click();
 		driver.findElement(By.xpath(
 				"//body/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/button[1]"))
 				.click();
+		driver.findElement(By.xpath(
+				"//body/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"))
+				.sendKeys("A1");
 		driver.findElement(By.xpath(
 				"//body/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[7]"))
 				.click();
+		//Clicando em Buscar
 		driver.findElement(By.xpath("//button[contains(text(),'Buscar')]")).click();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
--- src/test/java/homepage/HomePageTests.java
+++ src/test/java/homepage/HomePageTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import base.BaseTests;
 import pages.AnuncioPage;
 import pages.ListaAnunciosCarroPage;
+import suporte.Generator;
+import suporte.Screenshot;
 
 public class HomePageTests extends BaseTests {
 
@@ -17,12 +19,18 @@ public class HomePageTests extends BaseTests {
 		carregarPaginaInicial();
 		homePage.acessaLista();
 		assertThat(ListaAnunciosCarroPage.contarCarros(), is(20));
+		Screenshot.print(driver, "C:\\Users\\Estudo\\evidencia\\ValidaCarroUm\\" + Generator.dataHoraParaArquivo()
+		+ "testValidarDetalhesDoPrimeiroCarro_ModeloValorIguais" + ".png");
 
 	}
 
 	@Test
 	public void testValidarDetalhesDoPrimeiroCarro_ModeloValorIguais() {
 		homePage.acessaLista();
+
+		Screenshot.print(driver, "C:\\Users\\Estudo\\evidencia\\ValidaCarroUm\\" + Generator.dataHoraParaArquivo()
+				+ "testValidarDetalhesDoPrimeiroCarro_ModeloValorIguais" + ".png");
+
 		ListaAnunciosCarroPage.contarCarros();
 		int indiceCarro = 0;
 
@@ -30,32 +38,40 @@ public class HomePageTests extends BaseTests {
 		String precoLista = ListaAnunciosCarroPage.obterPreco(indiceCarro);
 
 		AnuncioPage anuncioPage = ListaAnunciosCarroPage.clicarAnuncio(indiceCarro);
+
 		String modeloCarro_AnuncioPage = anuncioPage.obterModeloCarro();
 		String precoCarro_AnuncioPage = anuncioPage.obterPreco();
 
 		assertThat(modeloCarroLista, CoreMatchers.containsString(modeloCarro_AnuncioPage));
 		assertThat(precoLista, CoreMatchers.containsString(precoCarro_AnuncioPage));
 
+		Screenshot.print(driver, "C:\\Users\\Estudo\\evidencia\\ValidaCarroUm\\" + Generator.dataHoraParaArquivo()
+				+ "testValidarDetalhesDoPrimeiroCarro_ModeloValorIguais" + ".png");
+
 	}
 
 	@Test
 	public void testValidarDetalhesDoSegundoCarro_ModeloValorIguais() {
 		homePage.acessaLista();
+
+		Screenshot.print(driver, "C:\\Users\\Estudo\\evidencia\\ValidaCarroDois\\" + Generator.dataHoraParaArquivo()
+				+ "testValidarDetalhesDoSegundoCarro_ModeloValorIguais" + ".png");
+
 		ListaAnunciosCarroPage.contarCarros();
 		int indiceCarroDois = 1;
 
 		String modeloCarroDoisLista = ListaAnunciosCarroPage.obterModeloCarro(indiceCarroDois);
 		String precoDoisLista = ListaAnunciosCarroPage.obterPreco(indiceCarroDois);
 
-
-
 		AnuncioPage anuncioPage = ListaAnunciosCarroPage.clicarAnuncio(indiceCarroDois);
 		String modeloCarroDois_AnuncioPage = anuncioPage.obterModeloCarro();
 		String precoCarroDois_AnuncioPage = anuncioPage.obterPreco();
-		
+
 		assertThat(modeloCarroDoisLista, CoreMatchers.containsString(modeloCarroDois_AnuncioPage));
 		assertThat(precoDoisLista, CoreMatchers.containsString(precoCarroDois_AnuncioPage));
 
+		Screenshot.print(driver, "C:\\Users\\Estudo\\evidencia\\ValidaCarroDois\\" + Generator.dataHoraParaArquivo()
+				+ "testValidarDetalhesDoSegundoCarro_ModeloValorIguais" + ".png");
 
 	}
 
